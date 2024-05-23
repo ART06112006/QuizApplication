91a880dcea8d54c0e65a72e8d22b0ace1d3c824a
ART06112006 <artem06112006@gmail.com>
2024-05-23 20:10:44 +0300

    Create AppDbContext.cs

---
+++ QuizApplication/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace QuizApplication.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        public static async Task<AppDbContext> AppDbContextAsync()
+        {
+            return await Task.Run(() => { return new AppDbContext(); });
+        }
+    }
+}
