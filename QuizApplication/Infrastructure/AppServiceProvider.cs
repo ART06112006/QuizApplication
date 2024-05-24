e5944021288de3bb4f51ca6a827e8071e8b55f00
ART06112006 <artem06112006@gmail.com>
2024-05-24 20:44:46 +0300

    Create AppServiceProvider.cs

---
+++ QuizApplication/Infrastructure/AppServiceProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApplication.Infrastructure
+{
+    public static class AppServiceProvider
+    {
+        public static ServiceProvider ServiceProvider { get; private set; }
+        public static void Initialize()
+        {
+            var services = new ServiceCollection();
+
+            
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+    }
+}
