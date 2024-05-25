9ce2a9583a4c04fd991669e2c304842ac694b0bc
ART06112006 <artem06112006@gmail.com>
2024-05-25 16:52:59 +0300

    Update AppServiceProvider.cs

--- QuizApplication/Infrastructure/AppServiceProvider.cs
+++ QuizApplication/Infrastructure/AppServiceProvider.cs
@@ -4,6 +4,8 @@ using QuizApplication.Context;
 using QuizApplication.Models;
 using QuizApplication.Repositories;
 using QuizApplication.Services;
+using QuizApplication.ViewModels;
+using QuizApplication.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +31,13 @@ namespace QuizApplication.Infrastructure
 
             //Repositories
             services.AddSingleton<QuizRepository>();
+
+            //Views
+            services.AddTransient<QuizWindow>();
+            services.AddTransient<QuizSettingsWindow>();
+
+            //ViewModels
+            services.AddTransient<QuizViewModel>();
 
             ServiceProvider = services.BuildServiceProvider();
         }
