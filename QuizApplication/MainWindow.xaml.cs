1d6b6444fab39c42a2bf6ef601e48c846c0e877c
ART06112006 <artem06112006@gmail.com>
2024-05-25 12:18:06 +0300

    Update APIService

---
+++ QuizApplication/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using QuizApplication.Infrastructure;
+using QuizApplication.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuizApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppServiceProvider.Initialize();
+            Do();
+        }
+        private async void Do()
+        {
+            var service = (APIQuestionService)AppServiceProvider.ServiceProvider.GetService(typeof(APIQuestionService));
+            await service.GetQuizAsync("fgdg", 50, new List<string>() { "easy", "hard" }, await service.GetCategoriesAsync());
+        }
+    }
+}
--- QuizApplication/Services/APIQuestionService.cs
+++ QuizApplication/Services/APIQuestionService.cs
@@ -1,25 +1,105 @@
-﻿using QuizApplication.Models;
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using QuizApplication.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace QuizApplication.Services
 {
     public class APIQuestionService
     {
-        public async Task<Quiz> GetQuizAsync()
+        private string categoriesUrl = "https://the-trivia-api.com/v2/categories";
+        private string questionsUrl = "https://the-trivia-api.com/v2/questions";
+
+        private readonly IMapper _mapper;
+
+        public Action<string> ExceptionMessage { get; set; }
+
+        public APIQuestionService(IMapper mapper) 
+        { 
+            _mapper = mapper;
+        }
+
+        public async Task<List<string>> GetCategoriesAsync()
         {
-            using (var httpClient = new HttpClient())
+            try
             {
-                var json = await httpClient.GetStringAsync("https://the-trivia-api.com/v2/quizzes");
-
-                if (json != null)
+                using (var client = new HttpClient())
                 {
+                    var response = await client.GetStringAsync(categoriesUrl);
 
+                    if (response != null)
+                    {
+                        var categories = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(response);
+
+                        var categoryList = new List<string>();
+                        foreach (var categoryGroup in categories.Values)
+                        {
+                            categoryList.AddRange(categoryGroup);
+                        }
+
+                        return categoryList;
+                    }
+                    else return null;
                 }
+            }
+            catch (HttpRequestException ex)
+            {
+                ExceptionMessage?.Invoke($"Request error: {ex.Message}");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                ExceptionMessage?.Invoke(ex.Message);
+                return null;
+            }
+        }
+
+        public async Task<Quiz> GetQuizAsync(string name, int questionLimit, List<string> difficultyLevels, List<string> categories)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var categoriesStr = string.Join(",", categories);
+                    var difficultiesStr = string.Join(",", difficultyLevels);
+
+                    var configuratedUrl = questionsUrl + $"?limit={questionLimit}&categories={categoriesStr}&difficulties={difficultiesStr}";
+
+                    var response = await client.GetStringAsync(configuratedUrl);
+
+                    if (response != null)
+                    {
+                        var questions = JsonConvert.DeserializeObject<List<APIQuestion>>(response);
+                        var mappedQuestions = _mapper.Map<List<Question>>(questions);
+
+                        return new Quiz()
+                        {
+                            Name = name,
+                            IsFinished = false,
+                            Questions = mappedQuestions,
+                            TotalScore = 0,
+                            UpdateDate = DateTime.Now
+                        };
+                    }
+                    else return null;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                ExceptionMessage?.Invoke($"Request error: {ex.Message}");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                ExceptionMessage?.Invoke(ex.Message);
+                return null;
             }
         }
     }
