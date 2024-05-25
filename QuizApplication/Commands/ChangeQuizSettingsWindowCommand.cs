92d53308d3a68861a70f675a2f95c215aedaf630
ART06112006 <artem06112006@gmail.com>
2024-05-25 16:52:55 +0300

    Add ChangeQuizSettingsViewCommand

---
+++ QuizApplication/Commands/ChangeQuizSettingsWindowCommand.cs
@@ -0,0 +1,19 @@
+﻿using QuizApplication.Infrastructure;
+using QuizApplication.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApplication.Commands
+{
+    public class ChangeQuizSettingsWindowCommand : BaseCommand
+    {
+        public override void Execute(object? parameter)
+        {
+            var window = (QuizSettingsWindow)(AppServiceProvider.ServiceProvider.GetService(typeof(QuizSettingsWindow)));
+            window.ShowDialog();
+        }
+    }
+}
