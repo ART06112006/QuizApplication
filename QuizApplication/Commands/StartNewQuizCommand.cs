3fce981af0789b6d0a4511978934fab9640d445f
ART06112006 <artem06112006@gmail.com>
2024-05-25 17:09:20 +0300

    Create StartNewQuizCommand.cs

---
+++ QuizApplication/Commands/StartNewQuizCommand.cs
@@ -0,0 +1,15 @@
+﻿using QuizApplication.Infrastructure;
+using QuizApplication.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApplication.Commands
+{
+    public class StartNewQuizCommand : BaseCommand
+    {
+        
+    }
+}
