230021f536174dd54187385a8c32cebcefe0b2ea
ART06112006 <artem06112006@gmail.com>
2024-05-27 16:14:16 +0300

    Correct Model Question

--- QuizApplication/Models/Question.cs
+++ QuizApplication/Models/Question.cs
@@ -11,14 +11,30 @@ namespace QuizApplication.Models
     {
         public int Id { get; set; }
         public string Category { get; set; }
-        public List<string> Tags { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
         public string Difficulty { get; set; }
         public string Text { get; set; }
         public string CorrectAnswer { get; set; }
-        public List<string> IncorrectAnswers { get; set; }
+        public IEnumerable<Answer> IncorrectAnswers { get; set; }
         public string Type { get; set; }
         public int QuizId { get; set; }
         public Quiz Quiz { get; set; }
         public string UserAnswer { get; set; }
+    }
+
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
+
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
     }
 }
