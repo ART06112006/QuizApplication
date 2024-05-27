d79f1214a6bbc46c5933cc387d599ee03d384b6c
ART06112006 <artem06112006@gmail.com>
2024-05-27 12:51:50 +0300

    Update Question.cs

--- QuizApplication/Models/Question.cs
+++ QuizApplication/Models/Question.cs
@@ -11,11 +11,11 @@ namespace QuizApplication.Models
     {
         public int Id { get; set; }
         public string Category { get; set; }
-        public IEnumerable<string> Tags { get; set; }
+        public List<string> Tags { get; set; }
         public string Difficulty { get; set; }
         public string Text { get; set; }
         public string CorrectAnswer { get; set; }
-        public IEnumerable<string> IncorrectAnswers { get; set; }
+        public List<string> IncorrectAnswers { get; set; }
         public string Type { get; set; }
         public int QuizId { get; set; }
         public Quiz Quiz { get; set; }
